30f48f4cfdaf9fc10665b6d4a54c69845b801bd9
Lee Eunjoo (Silver) <80297399+silver-eunjoo@users.noreply.github.com>
2025-06-16 10:40:39 +0900

    Test : FollowController 테스트 코드 작성

--- src/test/java/io/powerrangers/backend/utils/TestUtils.kt
+++ src/test/java/io/powerrangers/backend/utils/TestUtils.kt
@@ -1,6 +1,23 @@
 package io.powerrangers.backend.utils
-
+import io.powerrangers.backend.dto.UserFollowResponseDto
 import io.powerrangers.backend.entity.User
+
+fun genUserFollowResList(size: Int) : List<UserFollowResponseDto> {
+    var userList = mutableListOf<UserFollowResponseDto>()
+
+    for(i in 0 until size) {
+        userList.add(
+            UserFollowResponseDto(
+                id = i.toLong(),
+                nickname = "user_$i",
+                intro = "user_$i",
+                profileImage = null
+            )
+        )
+    }
+
+    return userList
+}
 
 fun genUserWithId(id: Long, nickname: String, email:String) =
     User(
